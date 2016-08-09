2555d83e3847f8fe14beadf0056bd0f4b14b9020
fangtang0101 <623003648@qq.com>
2016-08-09 17:02:06 +0800

    8.9
    刷新 + 更多  + 切换  ok

--- LYHWebApp/js/LYHMain.js
+++ LYHWebApp/js/LYHMain.js
@@ -1,6 +1,7 @@
 (function() {
 	//数据请求的相关模板
 	window.request_temp = {
+		//推荐（综合）
 		creatRequestDataForNews: function(pageIndex, pageSize) {
 			if (typeof(pageIndex)!="number") {
 				alert('您出传入的参数pageIndex必须是number')
@@ -10,22 +11,45 @@
 				alert('您出传入的参数pageSize必须是number')
 					return;			
 			}
-			
 			var jsonData = {"head":{"sid":"Model:iPod touch---Version:9.0.2","extensions":[{"name":"platform","value":"iOS"}],"cver":"2.0.2","cid":"61B711D4-0087-4C94-9D7F-2632A9BC43B5"},"filters":[{"items":[{"filterId":1}],"filterGroupId": 1}],"sort":{"pageIdx":0,"pageSize":25,"startTime":1469696344.835954}}
-
 			jsonData.sort.pageIdx = pageIndex;
 			jsonData.sort.pageSize = pageSize;
-//			
-//			alert(jsonData.sort.pageIdx);
-//			alert(jsonData.sort.pageSize);
-			
 			var obj = JSON.stringify(jsonData); 
-			
-//			alert(typeof(obj));
-
 			return obj;
-			
 		},
+		//公开课
+			creatRequestDataForNewsOfOpenClass: function(pageIndex, pageSize) {
+			if (typeof(pageIndex)!="number") {
+				alert('您出传入的参数pageIndex必须是number')
+					return;			
+			}
+			if (typeof(pageSize)!="number") {
+				alert('您出传入的参数pageSize必须是number')
+					return;			
+			}
+			var jsonData	 = {"head" : {"sid" : "Model:iPhone---Version:9.3","extensions" : [{"name" : "platform","value" : "iOS"}],"cver" : "2.0.5","cid" : "03D25FDC-CFF5-44D5-9662-3C957133C23D"},"filters" : [{"items" : [{"filterId" : 8}],"filterGroupId" : 1}],"sort" : {"pageIdx" : 0,"pageSize" : 25,"startTime" : 1470728140.433903}} 
+			jsonData.sort.pageIdx = pageIndex;
+			jsonData.sort.pageSize = pageSize;
+			var obj = JSON.stringify(jsonData); 
+			return obj;
+		},
+		//病例
+			creatRequestDataForNewsOfOfSick: function(pageIndex, pageSize) {
+			if (typeof(pageIndex)!="number") {
+				alert('您出传入的参数pageIndex必须是number')
+					return;			
+			}
+			if (typeof(pageSize)!="number") {
+				alert('您出传入的参数pageSize必须是number')
+					return;			
+			}
+			var jsonData	 = {"head" : {"sid" : "Model:iPhone---Version:9.3","extensions" : [{"name" : "platform","value" : "iOS"}],"cver" : "2.0.5","cid" : "444AD7D5-23AE-4DFB-942D-B19D2A7333D9"},"filters" : [{"items" : [{"filterId" : 10,"type" : 1}],"filterGroupId" : 1}],"sort" : {"pageIdx" : 0,"pageSize" : 25,"startTime" : 1470729208.550684}} 
+			jsonData.sort.pageIdx = pageIndex;
+			jsonData.sort.pageSize = pageSize;
+			var obj = JSON.stringify(jsonData); 
+			return obj;
+		},
+		
 	};
 
 	window.newsCell_tmpl = {
@@ -38,7 +62,6 @@
 				return;
 			}
 		},
-
 		//创item 模板
 		itemTmpl: function(obj) {
 			var imageUrl = obj.picUrl;
