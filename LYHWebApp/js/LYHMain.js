9421790f201e895dfb6fffe63f3bc9599a095880
fangtang0101 <623003648@qq.com>
2016-08-09 08:56:13 +0800

    8.9
    请求 string  加变量

--- LYHWebApp/js/LYHMain.js
+++ LYHWebApp/js/LYHMain.js
@@ -1,14 +1,34 @@
 (function() {
+	//数据请求的相关模板
+	window.request_temp = {
+
+		creatRequestDataForNews: function(pageIndex, pageSize) {
+			if (typeof(pageIndex)!="number") {
+				alert('您出传入的参数pageIndex必须是number')
+					return;			
+			}
+			
+			if (typeof(pageSize)!="number") {
+				alert('您出传入的参数pageSize必须是number')
+					return;			
+			}
+			var jsonData = '{"head":{"sid":"Model:iPod touch---Version:9.0.2","extensions":[{"name":"platform","value":"iOS"}],"cver":"2.0.2","cid":"61B711D4-0087-4C94-9D7F-2632A9BC43B5"},"filters":[{"items":[{"filterId":1}],"filterGroupId": 1}],"sort":{"pageIdx":0,"pageSize":25,"startTime":1469696344.835954}}'
+			return jsonData;
+		},
+
+	};
+
 	window.newsCell_tmpl = {
 		//点击订阅 推荐 公开课 病例
 		onClickChnanal: function(stringChannal) {
-			if (typeof(stringChannal)!="string") {
+			if(typeof(stringChannal) != "string") {
 				return;
 			}
-			if (stringChannal.length < 1) {
+			if(stringChannal.length < 1) {
 				return;
 			}
 		},
+
 		//创item 模板
 		itemTmpl: function(obj) {
 			var imageUrl = obj.picUrl;
